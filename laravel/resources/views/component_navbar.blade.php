61643aefd1f57188d80f0ddd1bb5b0ef0e6ecfd7
Helton Denis <heltondeniss@gmail.com>
2018-12-01 23:19:08 -0200

    Create migrations database

---
+++ laravel/resources/views/component_navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-ite ">
+                <a class="nav-link" href="/produtos">Produtos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/categorias">Categorias</a>
+            </li>
+        </ul>
+    </div>
+</nav>
